08d71ea9b7391476ad9a71a63623e1aa502fa197
kimhyera <khr1828@gmail.com>
2022-01-02 21:17:31 +0900

    update

--- src/app.jsx
+++ src/app.jsx
@@ -5,72 +5,72 @@ import Habits from './components/habits';
 import Navbar from './components/navbar';
 
 class App extends Component {
-  state = {
-    habits: [
-      { id: 1, name: '비타민 챙기기', count: 0 },
-      { id: 2, name: '운동 20분', count: 0 },
-      { id: 3, name: '물마시기', count: 0 },
-    ],
-  };
+	state = {
+		habits: [
+			{ id: 1, name: '비타민 챙기기', count: 0 },
+			{ id: 2, name: '운동 30분', count: 0 },
+			{ id: 3, name: '물마시기', count: 0 },
+		],
+	};
 
-  onIncrement = habit => {
-    const habits = this.state.habits.map(item => {
-      if (item.id === habit.id) {
-        return { ...habit, count: habit.count + 1 };
-      }
-      return item;
-    });
-    this.setState({ habits });
-  };
+	onIncrement = (habit) => {
+		const habits = this.state.habits.map((item) => {
+			if (item.id === habit.id) {
+				return { ...habit, count: habit.count + 1 };
+			}
+			return item;
+		});
+		this.setState({ habits });
+	};
 
-  onDecrement = habit => {
-    const habits = this.state.habits.map(item => {
-      if (item.id === habit.id) {
-        const count = habit.count - 1;
-        return { ...habit, count: count < 0 ? 0 : count };
-      }
-      return item;
-    });
-    this.setState({ habits });
-  };
+	onDecrement = (habit) => {
+		const habits = this.state.habits.map((item) => {
+			if (item.id === habit.id) {
+				const count = habit.count - 1;
+				return { ...habit, count: count < 0 ? 0 : count };
+			}
+			return item;
+		});
+		this.setState({ habits });
+	};
 
-  onDelete = habit => {
-    const habits = this.state.habits.filter(item => item.id !== habit.id);
-    this.setState({ habits });
-  };
+	onDelete = (habit) => {
+		const habits = this.state.habits.filter((item) => item.id !== habit.id);
+		this.setState({ habits });
+	};
 
-  onAdd = name => {
-    const habits = [...this.state.habits, { id: Date.now(), name, count: 0 }];
-    this.setState({ habits });
-  };
+	onAdd = (name) => {
+		const habits = [...this.state.habits, { id: Date.now(), name, count: 0 }];
+		this.setState({ habits });
+	};
 
-  onReset = () => {
-    const habits = this.state.habits.map(habit => {
-      if (habit.count !== 0) {
-        return { ...habit, count: 0 };
-      }
-      return habit;
-    });
-    this.setState({ habits });
-  };
+	onReset = () => {
+		const habits = this.state.habits.map((habit) => {
+			if (habit.count !== 0) {
+				return { ...habit, count: 0 };
+			}
+			return habit;
+		});
+		this.setState({ habits });
+	};
 
-  render() {
-    return (
-      <div className="container">
-        <Navbar
-          totalCount={this.state.habits.filter(item => item.count > 0).length}
-        />
-        <Habits
-          habits={this.state.habits}
-          onIncrement={this.onIncrement}
-          onDecrement={this.onDecrement}
-          onDelete={this.onDelete}
-          onAdd={this.onAdd}
-          onReset={this.onReset}
-        />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="container">
+				<Navbar
+					totalCount={this.state.habits.filter((item) => item.count > 0).length}
+				/>
+				<Habits
+					habits={this.state.habits}
+					onIncrement={this.onIncrement}
+					onDecrement={this.onDecrement}
+					onDelete={this.onDelete}
+					onAdd={this.onAdd}
+					onReset={this.onReset}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
